c620f63055efbd38abe0751b5831664cda5d02d5
dimmediato <immediatod1@gmail.com>
2024-03-26 03:54:59 -0400

    Added two new tabs
    Unfortunatly I forgot to keep up with saving images and skipped a
    commit, but this covers now two major updates. Pretty much I added two
    tabs, one encompasses the general trend in the decline of the liberal
    democracy index, grouped by country and the "regime type". The other tab
    is focused on two things, one is a regression analysis on
    populism/anti-pluralism and democratic backsliding, and the other is the
    rise of populism in recent years.
    
    The server side was much more complicated than the UI side. To briefly
    discuss what exactly was expanded upon compared to making the map,
    reactive graphs were added, including having them be clickable, with one
    displaying coordinates and the other displaying summary of data (a
    renderprint).

--- Shiny App/server.R
+++ Shiny App/server.R
@@ -1,5 +1,7 @@
 function(input, output, session){
   
+  # Server Map ----  
+  
   map_data <- reactive({switch(input$yearInput, 
                                "2023"=geo2$Year_2023,"2022"=geo2$Year_2022,"2021"=geo2$Year_2021,
                                "2020"=geo2$Year_2020,"2019"=geo2$Year_2019,"2018"=geo2$Year_2018,"2017"=geo2$Year_2017,
@@ -14,18 +16,10 @@ function(input, output, session){
                                "1984"=geo2$Year_1984,"1983"=geo2$Year_1983)
   })
   
-  pal <-colorNumeric(palette = c("#9e0142", "#d53e4f", "#f46d43", "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2"),
-                     domain=c(0,.09, .10,.19, .20,.29, .30,.39, .40,.49, .50,.59, .60,.69, .70,.79, .80,.89, .90,1))
-  mytext <- paste(
-    "Country: ", geo2$NAME,"<br/>", 
-    "Democracy Index: ", round(geo2$Year_2023, 2), 
-    sep="") %>%
-    lapply(htmltools::HTML)
-  
   output$map = renderLeaflet({ 
     leaflet(geo2) %>%
       setView( lat=10, lng=0 , zoom=2)%>%
-      addLegendNumeric(pal = pal, values = c(0,1), title = 'Democracy Index',
+      addLegendNumeric(pal = colorNumeric(palette = "RdYlBu", domain=NULL), values = c(0,1), title = 'Democracy Index',
                        orientation = 'horizontal', fillOpacity = .7, width = 150,
                        height = 20, position = 'bottomright', group = 'Symbols',
                        data = geo2) })
@@ -33,7 +27,7 @@ function(input, output, session){
   observe({
     leafletProxy("map", data=map_data()) %>% 
       addPolygons(data = geo2 , 
-                  fillColor = ~pal(map_data()), 
+                  fillColor = ~colorNumeric(palette = "RdYlBu", domain=NULL)(map_data()), 
                   fillOpacity = 0.7, 
                   weight = 0.2, 
                   smoothFactor = 0.2,
@@ -49,5 +43,114 @@ function(input, output, session){
                   ))
   })
   
+ # Democracy Index Plots, Grouped by Countries ---- 
   
+  output$plot1 <- renderPlot({
+    data <- V_Modified %>% filter(country_name %in% input$checkGroup1)
+    (ggplot(NULL, aes_string(x= "year", y = "v2x_libdem", group = "country_name")) + 
+      geom_line(data = data, aes(colour = country_name)) + 
+      labs(color='Country', x="Year", y= "Democracy Index"))
+ 
+  })
+  
+  output$info <- renderPrint({
+    req(input$plot_click1)
+    x <- round(input$plot_click1$x, 0)
+    y <- round(input$plot_click1$y, 2)
+    cat("[", x, ", ", y, "]", sep = "")
+  })
+  
+  output$plot2 <- renderPlot({
+    data2 <- V_Modified %>% filter(country_name %in% input$checkGroup2)
+    (ggplot(NULL, aes_string(x= "year", y = "v2x_libdem", group = "country_name")) + 
+        geom_line(data = data2, aes(colour = country_name)) + 
+        labs(color='Country', x="Year", y= "Democracy Index"))
+  })
+  
+  output$info2 <- renderPrint({
+    req(input$plot_click1)
+    x <- round(input$plot_click1$x, 0)
+    y <- round(input$plot_click1$y, 2)
+    cat("[", x, ", ", y, "]", sep = "")
+  })
+  
+  output$plot3 <- renderPlot({
+    data3 <- V_Modified %>% filter(country_name %in% input$checkGroup3)
+    (ggplot(NULL, aes_string(x= "year", y = "v2x_libdem", group = "country_name")) + 
+        geom_line(data = data3, aes(colour = country_name)) + 
+        labs(color='Country', x="Year", y= "Democracy Index"))
+  })
+  
+  output$info3 <- renderPrint({
+    req(input$plot_click1)
+    x <- round(input$plot_click1$x, 0)
+    y <- round(input$plot_click1$y, 2)
+    cat("[", x, ", ", y, "]", sep = "")
+  })
+  
+  # Party Regression Plots and Trend Bar Plot ---- 
+  
+  output$partytrend <- renderPlot({
+    yvarnames <- c("Populism" = "v2xpa_popul",
+                  "Anti-Pluralism" = "v2xpa_antiplural")
+    ggplot(merged_data, aes(x= year, y = .data[[input$radio]], fill = year)) + 
+      geom_bar(stat="identity") +
+      scale_fill_viridis_c(option = "plasma") + 
+      labs(fill='Year', x="Year", y= names(yvarnames[which(yvarnames == input$radio)]))
+  })
+  
+  filtered1 <- reactive({
+    merged_data %>% 
+      filter(v2pagovsup==input$govsupportcheck)
+  })
+  output$partygraph <- renderPlot({
+    ggplot(merged_data, aes(x = log(v2xpa_popul), y = v2x_libdem)) +
+      geom_point(data = filtered1(), aes(group = v2pagovsup)) + stat_smooth(data = filtered1(), aes(group = v2pagovsup, fill = v2pagovsup),method = "lm", 
+                                 formula = y ~ x, 
+                                 geom = "smooth", color = "red") + 
+      labs(fill = "Support Index", x="Party Populism", y= "Democracy Index")
+  })
+  
+  output$partyinfo <- renderPrint({
+    req(input$plot_click2)
+    if (input$govsupportcheck == "0") {
+    summary(lm(v2x_libdem~ log(v2xpa_popul), merged_data %>% 
+                 filter(v2pagovsup == "0")))} else
+    if (input$govsupportcheck == "1") {
+      summary(lm(v2x_libdem~ log(v2xpa_popul), merged_data %>% 
+                   filter(v2pagovsup == "1")))} else
+                     if (input$govsupportcheck == "2") {
+                       summary(lm(v2x_libdem~ log(v2xpa_popul), merged_data %>% 
+                                    filter(v2pagovsup == "2")))} else
+                                      if (input$govsupportcheck == "3") {
+                                        summary(lm(v2x_libdem~ log(v2xpa_popul), merged_data %>% 
+                                                     filter(v2pagovsup == "3")))}
+                     
+  })
+  
+  output$partygraph2 <- renderPlot({
+    ggplot(merged_data, aes(x = log(v2xpa_antiplural), y = v2x_libdem)) +
+      geom_point(data = filtered1(), aes(group = v2pagovsup)) + stat_smooth(data = filtered1(), aes(group = v2pagovsup, fill = v2pagovsup),method = "lm", 
+                                                                            formula = y ~ x, 
+                                                                            geom = "smooth", color = "red") + 
+      labs(fill = "Support Index", x="Party Anti-Pluralism", y= "Democracy Index")
+  })
+
+output$partyinfo2 <- renderPrint({
+  req(input$plot_click3)
+  if (input$govsupportcheck == "0") {
+    summary(lm(v2x_libdem~ log(v2xpa_antiplural), merged_data %>% 
+                 filter(v2pagovsup == "0")))} else
+                   if (input$govsupportcheck == "1") {
+                     summary(lm(v2x_libdem~ log(v2xpa_antiplural), merged_data %>% 
+                                  filter(v2pagovsup == "1")))} else
+                                    if (input$govsupportcheck == "2") {
+                                      summary(lm(v2x_libdem~ log(v2xpa_antiplural), merged_data %>% 
+                                                   filter(v2pagovsup == "2")))} else
+                                                     if (input$govsupportcheck == "3") {
+                                                       summary(lm(v2x_libdem~ log(v2xpa_antiplural), merged_data %>% 
+                                                                    filter(v2pagovsup == "3")))}
+  
+})
+
 }
