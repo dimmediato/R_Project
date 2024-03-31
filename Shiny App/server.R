0083a0d1c3e22d59c4188c62b0355b78be296b15
dimmediato <immediatod1@gmail.com>
2024-03-30 22:57:07 -0400

    Added polarization and inequality tabs
    The polarization tab had to be broken down because of the regions,
    resulting in a bloated amount of variables that essentially did the same
    thing just to extend the plot.The regression plot itself, though, was
    fairly simple. Clicking on each graph also displays the significance,
    which is helpful. The Gini was harder, requiring two lines to exist at
    once for each switch. This is why rbind was used. Otherwise it does the
    same thing.

--- Shiny App/server.R
+++ Shiny App/server.R
@@ -88,16 +88,25 @@ function(input, output, session){
     cat("[", x, ", ", y, "]", sep = "")
   })
   
-  # Party Regression Plots and Trend Bar Plot ---- 
+  # Party Trend Bar Plot ---- 
   
   output$partytrend <- renderPlot({
     yvarnames <- c("Populism" = "v2xpa_popul",
                   "Anti-Pluralism" = "v2xpa_antiplural")
-    ggplot(merged_data, aes(x= year, y = .data[[input$radio]], fill = year)) + 
-      geom_bar(stat="identity") +
-      scale_fill_viridis_c(option = "plasma") + 
+    ggplot(average_merge, aes(x= year, y = .data[[input$radio]], fill = year)) + 
+      geom_bar(stat="identity") +  geom_line() +
+      scale_fill_viridis_c(option = "plasma") + coord_cartesian(ylim = c(.2, .6)) +
       labs(fill='Year', x="Year", y= names(yvarnames[which(yvarnames == input$radio)]))
   })
+  
+  output$trend <- renderPrint({
+    req(input$party_trendclick)
+    x <- round(input$party_trendclick$x, 0)
+    y <- input$party_trendclick$y
+    cat("[", x, ", ", y, "]", sep = "")
+  })
+  
+  # Party Regression Plots ----
   
   filtered1 <- reactive({
     merged_data %>% 
@@ -151,6 +160,125 @@ output$partyinfo2 <- renderPrint({
                                                        summary(lm(v2x_libdem~ log(v2xpa_antiplural), merged_data %>% 
                                                                     filter(v2pagovsup == "3")))}
   
+})
+
+# Polarization Trend Bar Plot ---- 
+
+output$polarizationtrend <- renderPlot({
+polarizationdata <- V_Modified %>% filter(country_name %in% input$polarizationpicker)
+polarizationdata2 <- V_Modified %>% filter(country_name %in% input$polarizationpicker2)
+polarizationdata3 <- V_Modified %>% filter(country_name %in% input$polarizationpicker3)
+polarizationdata4 <- V_Modified %>% filter(country_name %in% input$polarizationpicker4)
+polarizationdata5 <- V_Modified %>% filter(country_name %in% input$polarizationpicker5)
+polarizationdata6 <- V_Modified %>% filter(country_name %in% input$polarizationpicker6)
+
+ggplot(V_Modified, aes(x=year, y=v2cacamps_osp)) +
+  geom_smooth(data = polarizationdata, method = loess, formula = y ~ x, aes(colour = country_name)) + 
+  geom_smooth(data = polarizationdata2, method = loess, formula = y ~ x, aes(colour = country_name)) + 
+  geom_smooth(data = polarizationdata3, method = loess, formula = y ~ x, aes(colour = country_name)) + 
+  geom_smooth(data = polarizationdata4, method = loess, formula = y ~ x, aes(colour = country_name)) +
+  geom_smooth(data = polarizationdata5, method = loess, formula = y ~ x, aes(colour = country_name)) + 
+  geom_smooth(data = polarizationdata6, method = loess, formula = y ~ x, aes(colour = country_name)) +
+  labs(color='Country', x="Year", y= "Political Polarization")
+})
+
+output$polarizationinfo <- renderPrint({
+  req(input$polarization_click)
+  x <- round(input$polarization_click$x, 0)
+  y <- round(input$polarization_click$y, 2)
+  cat("[", x, ", ", y, "]", sep = "")
+})
+
+# Polarization Regression Plots ---- 
+
+filtered2 <- reactive({
+  V_Modified %>% 
+    filter(e_regionpol_6C==input$radio2)
+})
+output$polarizationregression <- renderPlot({
+  ggplot(V_Modified, aes(x = v2cacamps_osp, y = v2x_libdem)) +
+    geom_point(data = filtered2(), aes(group = e_regionpol_6C)) + geom_smooth(data = filtered2(), aes(group = e_regionpol_6C, fill = e_regionpol_6C),method = "lm", 
+                                                                          formula = y ~ x, 
+                                                                          color = input$radio2) + 
+    labs(fill = "Region Index", x="Political Polarization", y= "Democracy Index")
+})
+
+output$polarizationinfo2 <- renderPrint({
+  req(input$polarization_click2)
+  if (input$radio2 == "0") {
+    summary(lm(v2x_libdem~ v2cacamps_osp, V_Modified %>% 
+                 filter(e_regionpol_6C== "0")))} else
+                   if (input$radio2 == "1") {
+                     summary(lm(v2x_libdem~ v2cacamps_osp, V_Modified %>% 
+                                  filter(e_regionpol_6C == "1")))} else
+                                    if (input$radio2 == "2") {
+                                      summary(lm(v2x_libdem~ v2cacamps_osp, V_Modified %>% 
+                                                   filter(e_regionpol_6C == "2")))} else
+                                                     if (input$radio2 == "3") {
+                                                       summary(lm(v2x_libdem~ v2cacamps_osp, V_Modified %>% 
+                                                                    filter(e_regionpol_6C == "3")))} else if (input$radio2 == "4") {
+                                                                      summary(lm(v2x_libdem~ v2cacamps_osp, V_Modified %>% 
+                                                                                   filter(e_regionpol_6C == "4")))}
+  else if (input$radio2 == "5") {
+    summary(lm(v2x_libdem~ v2cacamps_osp, V_Modified %>% 
+                 filter(e_regionpol_6C == "5")))}
+  else if (input$radio2 == "6") {
+    summary(lm(v2x_libdem~ v2cacamps_osp, V_Modified %>% 
+                 filter(e_regionpol_6C == "6")))}
+})
+
+output$GINItrend <- renderPlot({
+  GINIdata <- merged_GINI %>% filter(country_name %in% input$GINIpicker)
+  GINIdata2 <- merged_GINI %>% filter(country_name %in% input$GINIpicker2)
+  GINIdata3 <- merged_GINI %>% filter(country_name %in% input$GINIpicker3)
+  GINIdata4 <- merged_GINI %>% filter(country_name %in% input$GINIpicker4)
+  GINIdata5 <- merged_GINI %>% filter(country_name %in% input$GINIpicker5)
+  GINIdata6 <- merged_GINI %>% filter(country_name %in% input$GINIpicker6)
+  GINI_all <- rbind(GINIdata, GINIdata2, GINIdata3, GINIdata4, GINIdata5, GINIdata6)
+  
+  ggplot(merged_GINI, aes(x=year) )  +
+    geom_smooth(data=GINI_all, method = loess, formula = y ~ x, aes(y=Value, color = "GINI Score")) + 
+    geom_smooth(data=GINI_all, method = loess, formula = y ~ x, aes(y=v2x_libdem*100, color = "Democracy Index")) + 
+    scale_colour_manual(name="Legend", values=c("darkblue", "darkgreen")) +
+    labs(x="Year", y= "Dual GINI and Democracy Levels") 
+})
+
+# GINI Regression Plots ---- 
+
+filtered3 <- reactive({
+  merged_GINI %>% 
+    filter(e_regionpol_6C==input$radio3)
+})
+output$GINIregression <- renderPlot({
+  ggplot(merged_GINI, aes(x = Value, y = v2x_libdem)) +
+    geom_point(data = filtered3(), aes(group = e_regionpol_6C)) + geom_smooth(data = filtered3(), aes(group = e_regionpol_6C, fill = e_regionpol_6C),method = "loess", 
+                                                                              formula = y ~ x, 
+                                                                              color = input$radio3) + 
+    labs(fill = "Region Index", x="GINI Coef", y= "Democracy Index")
+})
+
+output$GINIinfo2 <- renderPrint({
+  req(input$GINI_regression)
+  if (input$radio3 == "0") {
+    summary(lm(v2x_libdem~ Value, merged_GINI %>% 
+                 filter(e_regionpol_6C== "0")))} else
+                   if (input$radio3 == "1") {
+                     summary(lm(v2x_libdem~ Value, merged_GINI %>% 
+                                  filter(e_regionpol_6C == "1")))} else
+                                    if (input$radio3 == "2") {
+                                      summary(lm(v2x_libdem~ Value, merged_GINI %>% 
+                                                   filter(e_regionpol_6C == "2")))} else
+                                                     if (input$radio3 == "3") {
+                                                       summary(lm(v2x_libdem~ Value, merged_GINI %>% 
+                                                                    filter(e_regionpol_6C == "3")))} else if (input$radio3 == "4") {
+                                                                      summary(lm(v2x_libdem~ Value, merged_GINI %>% 
+                                                                                   filter(e_regionpol_6C == "4")))}
+  else if (input$radio3 == "5") {
+    summary(lm(v2x_libdem~ Value, merged_GINI %>% 
+                 filter(e_regionpol_6C == "5")))}
+  else if (input$radio3 == "6") {
+    summary(lm(v2x_libdem~ Value, merged_GINI %>% 
+                 filter(e_regionpol_6C == "6")))}
 })
 
 }
